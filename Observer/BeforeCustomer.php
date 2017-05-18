347be5916fc8f75087e5440e4efdd3c294f74a0a
Jefferson Porto <jefferson.porto@ymail.com>
2017-05-18 01:28:42 -0300

    add validate customer backend

--- Observer/BeforeCustomer.php
+++ Observer/BeforeCustomer.php
@@ -6,6 +6,8 @@ use Magento\Framework\Event\ObserverInterface;
 
 use Magento\Framework\Event\Observer as EventObserver;
 
+use Magento\TestFramework\Event\Magento;
+
 class BeforeCustomer implements ObserverInterface
 {
 
@@ -15,11 +17,20 @@ class BeforeCustomer implements ObserverInterface
     
     protected $_messageManager = null;
     
+    protected $_customer = null;
+    
+    protected $_storeManager = null;
+    
     public function __construct(
         \Magento\Framework\Message\ManagerInterface $messageManager,
         \Jbp\Customer\Helper\Taxvat $taxvat,
-        \Psr\Log\LoggerInterface $logger
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Customer\Model\CustomerFactory $customer,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customer
     ) {
+        
+        $this->_storeManager = $storeManager;
         
         $this->_taxvat = $taxvat;
         
@@ -27,30 +38,27 @@ class BeforeCustomer implements ObserverInterface
         
         $this->_logger = $logger;
         
+        $this->_customer = $customer->create();
+        
     }
     
     public function execute(EventObserver $observer)
     {
-        
         $taxvat = $observer->getEvent()->getDataObject()->getTaxvat();
+        
+        $rgStateenrollment = $observer->getEvent()->getDataObject()->getData('rg_stateenrollment');
+        
+        $email = $observer->getEvent()->getDataObject()->getEmail();
         
         if (!empty($taxvat)) {
             
-            $this->_taxvat->setTaxvat($taxvat);
+            $taxvat = $this->_isValidTaxVat($taxvat);
             
-            $valida = $this->_taxvat->valida();
+            $observer->getEvent()->getDataObject()->setRgStateenrollment(preg_replace('/\D/', '', $rgStateenrollment));
             
-            if (!$valida) {
-                
-                $type = $this->_getTypeDocument($this->_taxvat->verifica_cpf_cnpj());
-                
-                $this->_messageManager->addNotice($this->_getMessage($type));
-                
-                $this->_logger->info($this->_getMessage($type));
-                
-                throw new \Exception($this->_getMessage($type));
-                
-            }
+            $observer->getEvent()->getDataObject()->setTaxvat($taxvat);
+            
+            $this->_isTaxvatExists($taxvat, $rgStateenrollment, $email);
             
         }
         
@@ -58,9 +66,67 @@ class BeforeCustomer implements ObserverInterface
         
     }
     
+    protected function _isRequiredSex($typePerson)
+    {
+        
+    }
+    
+    protected function _isRequiredDob($typePerson)
+    {
+        
+    }
+    
+    protected function _isValidTaxVat($taxvat)
+    {
+        $this->_taxvat->setTaxvat($taxvat);
+        
+        $valida = $this->_taxvat->valida();
+        
+        if (!$valida) {
+            
+            $this->_messageManager->addNotice(
+                $this->_getMessage('taxvatInvalid',
+                    $this->_taxvat->verifica_cpf_cnpj()));
+            
+            throw new \Exception($this->_getMessage(
+                'taxvatInvalid', $this->_taxvat->verifica_cpf_cnpj()));
+            
+        }
+        
+        return $this->_taxvat->formata();
+        
+    }
+    
+    protected function _isTaxvatExists($taxvat, $rgStateenrollment,  $email)
+    {
+        $collection = $this->_customer
+                           ->addAttributeToFilter(
+                               array(
+                                   array('attribute' => 'taxvat', 'eq' => $taxvat),
+                                   array('attribute' => 'rg_stateenrollment', 'eq' => $rgStateenrollment),
+                               )
+                           )
+                           ->addAttributeToFilter('rg_stateenrollment', array('notnull' => true))
+                           ->addAttributeToFilter('email', array('neq' => $email))
+                           ->addAttributeToFilter('store_id', array('eq' => $this->_storeManager->getStore()->getId()))
+                           ->addAttributeToFilter('website_id', array('eq' => $this->_storeManager->getStore()->getWebsiteId()));
+        
+                           
+        if ($collection->getSize()) {
+            
+            $this->_messageManager->addNotice(
+                $this->_getMessage('taxvatExists',
+                    $this->_taxvat->verifica_cpf_cnpj()));
+            
+            throw new \Exception($this->_getMessage('taxvatExists', $taxvat));
+        }
+        
+        return $this;
+    }
+    
     protected function _getTypeDocument($taxvat)
     {
-        if ($taxvat === false) {            
+        if ($taxvat === false) {
             return __('Taxvat');            
         }
         
@@ -68,9 +134,21 @@ class BeforeCustomer implements ObserverInterface
         
     }
     
-    protected function _getMessage($type)
+    protected function _getMessage($type, $taxvat)
     {
-        return __("Error {$type} invalid");
+        $taxvat= $this->_getTypeDocument($taxvat);
+        
+        switch ($type) {
+            case 'taxvatExists':
+                return __(($taxvat == 'CPF' ? $taxvat.'or RG' : $taxvat.'or IE')." is registered click <a href='/customer/account/forgotpassword'>Here</a> for recover password");
+            break;
+            
+            case 'taxvatInvalid':
+                return __("Error {$taxvat} invalid");
+            break;
+        }
+        
+        
     }
     
 }
--- Plugin/CustomerDatePlugin.php
+++ Plugin/CustomerDatePlugin.php
@@ -2,20 +2,11 @@
 
 namespace Jbp\Customer\Plugin;
 
-use Magento\Framework\Message\ManagerInterface as MessageManager;
-
 class CustomerDatePlugin
 {
-    
-    private $messageManager;
-    
-    public function __construct(MessageManager $messageManager){        
-        $this->messageManager = $messageManager;
-    }
-    
     public function beforeValidateValue($value)
     {      
-        $this->messageManager->addSuccessMessage("teste");      
+        die();      
     }
     
 }
